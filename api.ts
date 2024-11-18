cc9aea8b89a4902db8c1cb1d4cdb5e7b432a5c0a
Aryan <aryantrivedi.lko@outlook.com>
2024-11-18 15:34:53 +0530

    express

--- api.ts
+++ api.ts
@@ -1,40 +1,45 @@
-import { Hono } from 'hono'
+import express, { Request, Response } from 'express'
 import { fetchCodeforcesData } from './platform-services/codeforces'
 import { fetchGeeksForGeeksData } from './platform-services/geeksforgeeks'
 import { fetchCodeChefData } from './platform-services/codechef'
 import { fetchLeetCodeData } from './platform-services/leetcode'
-const app = new Hono()
 
+const app = express()
+
+// Middleware to parse JSON requests
+app.use(express.json())
 
 // Fetch data from the platform based on the platform and username
-app.get('/fetch/:platform/:username', async (c) => {
-  const { platform, username } = c.req.param()
+app.get('/fetch/:platform/:username', async (req: Request, res: Response) => {
+  const { platform, username } = req.params
 
   if (platform === 'codeforces') {
     const data = await fetchCodeforcesData(username)
-    return c.json(data)
+    return res.json(data)
   } else if (platform === 'geeksforgeeks') {
     const data = await fetchGeeksForGeeksData(username)
-    return c.json(data)
+    return res.json(data)
   } else if (platform === 'codechef') {
     const data = await fetchCodeChefData(username)
-    return c.json(data)
-  } else if(platform === 'leetcode'){
+    return res.json(data)
+  } else if (platform === 'leetcode') {
     const data = await fetchLeetCodeData(username)
-    return c.json(data)
+    return res.json(data)
   } else {
-    return c.json({ message: 'Invalid platform' })
+    return res.json({ message: 'Invalid platform' })
   }
 })
 
 // Fetch data from all platforms based on the username
-app.get('/fetch-all/:username', async (c) => {
-  const username = c.req.param('username')
+app.get('/fetch-all/:username', async (req: Request, res: Response) => {
+  const { username } = req.params
+
   const codeforcesData = await fetchCodeforcesData(username)
   const geeksForGeeksData = await fetchGeeksForGeeksData(username)
   const codeChefData = await fetchCodeChefData(username)
   const leetCodeData = await fetchLeetCodeData(username)
-  return c.json({ codeforces: codeforcesData, geeksforgeeks: geeksForGeeksData, codechef: codeChefData, leetcode: leetCodeData })
+
+  return res.json({ codeforces: codeforcesData, geeksforgeeks: geeksForGeeksData, codechef: codeChefData, leetcode: leetCodeData })
 })
 
 export default app
